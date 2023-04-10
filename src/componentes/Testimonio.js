4ad237329b02b30ccf38f753b2b7d57d2cdb275e
Juan Penalver <122556197+JuanPenalver11@users.noreply.github.com>
2023-04-10 16:04:45 +0200

    comentarios-class-react

--- src/componentes/Testimonio.js
+++ src/componentes/Testimonio.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import '../hojas-de-estilo/testimonio.css';
 
-function Testimonio(props) {
-  return (
-    <div className='contenedor-testimonio'>
-      <img className='image-testimonio' src={require(`../imagenes/testimonio-${props.imagen}.avif`)}
-        alt='Foto Rubia' />
+
+class Testimonio extends React.Component{
+  render(){
+    return(
+      <div className='contenedor-testimonio'>
+      <img className='image-testimonio' src={require(`../imagenes/testimonio-${this.props.imagen}.avif`)}
+        alt={`Foto de ${this.props.nombre}`}/>
       <div className='contendor-texto-testimonio'>
-        <p className='nombre-testimonio'><strong>{props.nombre}</strong> de {props.pais}</p>
-        <p className='cargo-testimonio'><strong>{props.trabajo} </strong>en {props.empresa}</p>
-        <p className='texto-testimonio'>"{props.testimonio}"</p>
+        <p className='nombre-testimonio'><strong>{this.props.nombre}</strong> de {this.props.pais}</p>
+        <p className='cargo-testimonio'><strong>{this.props.trabajo} </strong>en {this.props.empresa}</p>
+        <p className='texto-testimonio'>"{this.props.testimonio}"</p>
       </div>
     </div>
-  );
-};
+    )
+  }
+}
+
 
 export default Testimonio;
 
